063058f5db8eeb2fcdd7c3293dd61aef1718853d
Ryan Victor <91751440+RVFB12345@users.noreply.github.com>
2022-04-12 18:07:16 -0300

    Add files via upload

---
+++ JavaScript-cadastro.js
@@ -0,0 +1,31 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+
+
+
+const inputs = document.getElementsByClassName("inputUser");
+for(const input of inputs) {
+    input.addEventListener("blur", function(){
+        if(input.value.trim() != ""){
+            input.classList.add("has-val");
+        } else {
+            input.classList.remove("has-val");
+        }
+    });
+}
